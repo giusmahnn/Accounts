185e704c370e4f341416e3c12ff3f71595cd6df7
Remigius Mgbeme <garethremigius@gmail.com>
2024-06-05 17:48:06 +0100

    updated_templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, logout, authenticate
+#from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+
+
+def login_view(request):
+    pass
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+    return redirect('home')
